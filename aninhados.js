3f413a8b67213f687fcde1b8047cdcd5168b366e
kauacostasantos05-lab <kaua.costa.santos05@escola.pr.gov.br>
2026-08-27 15:20:19 +0000

    aninhados

---
+++ aninhados.js
@@ -0,0 +1,15 @@
+const cliente = {
+    nome: "joao",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["115555550", "114444440"],
+};
+
+cliente.endereco = {
+    Rua: "R. Jose Climber",
+    numero: 123,
+    apartamento: true,
+    complemento : "ap934",
+};
+
+console.log(cliente);
---
+++ tipos.js
@@ -0,0 +1,8 @@
+const cliente = {
+    nome: "joao",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["115555550", "114444440"],
+};
+
+    console.log(cliente.telefone[1]);
