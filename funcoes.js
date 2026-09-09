bebedc684213d370ae1fe638480898344f3efd2b
kauacostasantos05-lab <kaua.costa.santos05@escola.pr.gov.br>
2026-09-09 14:50:32 +0000

    save

--- funcoes.js
+++ funcoes.js
@@ -5,8 +5,12 @@ const cliente = {
     telefone: ["115555550", "114444440"],
     saldo: 200,
     efetuaPagamento: function(valor) {
-        if (valor > this.save){
+        if (valor > this.saldo){
             console.log("Saldo Insuficiente");
+        } else {
+            this.saldo -= valor;
+            console.log(`pagamento efetuado. Novo saldo: ${this.saldo}`);
         }
-    }
+    },
 };
+cliente.efetuaPagamento(250);
